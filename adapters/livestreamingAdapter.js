1fd10f8c0ecdbc9e52db8d39e243d59e07a97b53
cricketscorelive <livecrickettvscore@gmail.com>
2025-12-14 23:51:20 +0530

    Add files via upload

--- adapters/livestreamingAdapter.js
+++ adapters/livestreamingAdapter.js
@@ -1,21 +1,29 @@
 import { COLORS } from "../config/colors.js";
 
-export function LiveStreamingAdapter(list, onItemClick) {
+export function LiveStreamingAdapter(list) {
   return list.map(item => `
     <div class="card" onclick='window.open("${item.url}", "_blank")'>
-      
+
       <div class="title">${item.title}</div>
 
       <div class="teams">
         <div class="team">
-          <img src="${item.team1_img}" />
+          <img 
+            src="${item.team1_img}" 
+            referrerpolicy="no-referrer"
+            onerror="this.src='https://via.placeholder.com/40'"
+          />
           <span>${item.team1}</span>
         </div>
 
         <div class="vs">VS</div>
 
         <div class="team">
-          <img src="${item.team2_img}" />
+          <img 
+            src="${item.team2_img}" 
+            referrerpolicy="no-referrer"
+            onerror="this.src='https://via.placeholder.com/40'"
+          />
           <span>${item.team2}</span>
         </div>
       </div>
